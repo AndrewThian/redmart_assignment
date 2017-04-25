9f1e18b9a39dddf2887ee51194ce86e2b226ed53
AndrewThian <andrewbobbythian@gmail.com>
2017-04-25 11:12:52 +0800

    implement component props for each contact and bootstrap styling for contact cards

---
+++ src/components/contacts/Contact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          CONTACT NAME
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <p>CONTACT EMAIL</p>
+          <p>CONTACT NUMBER</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- src/components/contacts/Contacts.vue
+++ src/components/contacts/Contacts.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1>Contacts</h1>
+  <div>
+    <app-contact
+      v-for="contact in contacts"
+      :contact="contact"
+      :key="contact.email"
+      ></app-contact>
+  </div>
 </template>
 
 <script>
+import Contact from './Contact.vue'
+
 export default {
+  data () {
+    return {
+      contacts: [
+        { name: 'Andrew Thian', email: 'andrew@gmail.com', number: 91234567 },
+        { name: 'John Doe', email: 'john@gmail.com', number: 91234567 },
+        { name: 'Jane Doe', email: 'jane@gmail.com', number: 91234567 },
+        { name: 'Jean Grey', email: 'jean@gmail.com', number: 91234567 },
+        { name: 'Scott Summers', email: 'scott@gmail.com', number: 91234567 }
+      ]
+    }
+  },
+  components: {
+    appContact: Contact
+  }
 }
 </script>
 
